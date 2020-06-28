a72c8e6132e21cb0e5186cceafbd5dec8c0e50e2
Abdelwahab07 <abdelwahab.huss07@gmail.com>
2020-06-28 17:11:36 +0200

    Add UVA-408

---
+++ Arabic-Competitive-Sheet/CF-A/Math/black_and_white_painting.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double n = 1, m = 1, white;
+    std::cin >> n >> m >> white;
+
+    while (n && m) {
+        double mi = std::min(n, m);
+        double ma = std::max(n, m);
+
+        double rest = 0;
+        if (!white) {
+            rest = ceil(mi - 8.0) / 2.0;
+            ma--;
+        }
+
+        double x = (mi - 8) + 1;
+        double y = (ma - 8) + 1;
+        double result = ((x * y) / 2) + rest;
+
+        long int finalResult = ceil(result);
+        std::cout << finalResult << "\n";
+
+        std::cin >> n >> m >> white;
+    }
+}
